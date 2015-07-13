abc1aabcd92d93a4cd80586882af93f4b12f09ca
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-13 11:20:10 -0500

    ryan while loop update

--- while.php
+++ while.php
@@ -3,10 +3,14 @@
 $test = 5;
 
 while ($test <= 15){
-	echo $test;
-	$test++; 
+	if ($test % 2 == 0 && $test % 3 == 0){
+		echo "{$test} is divisible by two and three" . PHP_EOL;
+	} else if ($test % 2 == 0){
+		echo "{$test} is divisible by two" . PHP_EOL;
+	} else {
+		echo $test . PHP_EOL; 
+	}
+	$test += 1;
 }
-
-
 
 ?>
