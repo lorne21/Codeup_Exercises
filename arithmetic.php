9889e5ffca46e26303f72da54b6acf159b44fbc2
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-15 09:18:01 -0500

    arithmetic php and intro php with project euler solutions

---
+++ arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b)
+{
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b)
+{
+    echo $a - $b . PHP_EOL; 
+}
+
+function multiply($a, $b)
+{
+    echo $a * $b . PHP_EOL; 
+}
+
+function divide($a, $b)
+{
+    echo $a / $b . PHP_EOL; 
+}
+
+// Add code to test your functions here
+add(2, 3);
+subtract(9, 7);
+multiply(2 , 3);
+divide(10, 2);
--- intro.php
+++ intro.php
@@ -1,18 +1,55 @@
 <?php
 
-$name = 'Lorne';
-echo "Hello World" . PHP_EOL; 
-echo "My name is $name" . PHP_EOL; 
+// this outputs name
+// $name = 'Lorne';
+// echo "Hello World" . PHP_EOL; 
+// echo "My name is $name" . PHP_EOL; 
 
-$numbercount = [];
-for ($i = 1; $i < 1000; $i++){
-	if ($i % 5 == 0){
-		array_push($numbercount , $i);
-	} else if ($i % 3 == 0){
-		array_push($numbercount , $i);
-	} 
-}  
+// this gets sum of all divisible by 5 or 3
+// $numbercount = [];
+// for ($i = 1; $i < 1000; $i++){
+// 	if ($i % 5 == 0){
+// 		array_push($numbercount , $i);
+// 	} else if ($i % 3 == 0){
+// 		array_push($numbercount , $i);
+// 	} 
+// }  
 
-echo array_sum($numbercount) . PHP_EOL; 
+// echo array_sum($numbercount) . PHP_EOL; 
+
+// this function get fibonacci
+// $fib = 1; 
+// $a = 0;
+// $b = 1; 
+// $fibArray = []; 
+// for ($i = 0; $fib < 4000000; $i++){
+// 	$fib = $a + $b; 
+// 	// echo $fib . PHP_EOL; 
+// 	$a = $b; 
+// 	$b = $fib;
+// 	if ($fib % 2 == 0){	
+// 		array_push($fibArray , $fib);
+// 	}
+// }
+
+// echo array_sum($fibArray) . PHP_EOL; 
+
+// this gets largest gmp_prob_prime 
+
+$a = 600851475143; 
+for ($i = 2; $i <= $a; $i++) {
+	if ($i % 2 != 0){
+		$isPrime = $a / $i; 
+		if (is_integer($isPrime) == true){
+			echo $isPrime . PHP_EOL;
+		} 
+	}
+	
+}
+
+
+
+
+
 
 ?>
