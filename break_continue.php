10bc38a4a8bc0885de6d339eeaf610f1f86e782d
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-15 10:38:53 -0500

    break continue revision and internal functions

--- break_continue.php
+++ break_continue.php
@@ -1,17 +1,18 @@
 <?php
 
-for ($i = 0; $i <= 100; $i++){
+for ($i = 1; $i <= 100; $i++){
 	if ($i % 2 != 0){
-		echo "$i\n";
-	}
+		continue;
+		
+	} echo "$i\n";
 }
 
-for ($i = 1; $i <= 100; $i++){
-	echo "$i\n";
-	if ($i == 10){
-		break; 
-	}
-}
+// for ($i = 1; $i <= 100; $i++){
+// 	echo "$i\n";
+// 	if ($i == 10){
+// 		break; 
+// 	}
+// }
 
 
 
