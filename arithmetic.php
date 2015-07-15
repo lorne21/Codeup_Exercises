15f8a502d00bb649ebe12e8869d4f5e18200f91c
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-15 10:21:34 -0500

    arithmetic php

--- arithmetic.php
+++ arithmetic.php
@@ -1,27 +1,49 @@
 <?php
 
-function add($a, $b)
-{
-    echo $a + $b . PHP_EOL;
+function add($a, $b){
+	errorCheck($a, $b); 
+	echo $a + $b . PHP_EOL; 
 }
 
-function subtract($a, $b)
-{
+function subtract($a, $b){
+	errorCheck($a, $b);
     echo $a - $b . PHP_EOL; 
 }
 
-function multiply($a, $b)
-{
+function multiply($a, $b){
+	errorCheck($a, $b);
     echo $a * $b . PHP_EOL; 
 }
 
-function divide($a, $b)
-{
+function divide($a, $b){
+	errorCheck($a, $b);
     echo $a / $b . PHP_EOL; 
 }
 
+function modulous($a , $b){
+	errorCheck($a, $b);
+	echo $a % $b . PHP_EOL; 
+}
+
+function errorCheck($a , $b){
+	if (is_numeric($a) && is_numeric($b)){
+	} else {
+		echo "Both arguments must be numbers" . PHP_EOL; 
+		echo "Your first argument was a " . gettype($a) . ". Your second argument was a " . gettype($b) . "." . PHP_EOL; 
+		exit; 
+	}
+
+	if ($b == 0){
+		echo "Your second argument cannot be zero.";
+	}
+
+
+
+}
+
 // Add code to test your functions here
-add(2, 3);
-subtract(9, 7);
-multiply(2 , 3);
-divide(10, 2);
+// add(1 , 4);
+// subtract(9, 7);
+// multiply(2 , 3);
+divide(10, 3);
+// modulous(10, 2);
